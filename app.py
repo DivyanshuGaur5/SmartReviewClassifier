17b215fd977a6f0cca118e66d59235aa58d3dea5
DivyanshuGaur5 <148606824+DivyanshuGaur5@users.noreply.github.com>
2025-07-31 09:49:14 +0530

    added reviewer frequency

--- app.py
+++ app.py
@@ -6,13 +6,12 @@ from scipy.sparse import hstack
 
 model = joblib.load('model.pkl')
 vectorizer = joblib.load('vectorizer.pkl')
-scaler = joblib.load('scaler.pkl')
 
 st.title("Smart Review Filter")
 st.subheader("Detect whether a product review is genuine or fake")
 
-user_review = st.text_area("Enter your review here:")  
-review_freq = 1
+user_review = st.text_area("Enter your review here:")
+is_frequent = st.checkbox("Is the reviewer a frequent reviewer?", value=False)
 
 if st.button("Predict"):
     if not user_review.strip():
@@ -20,11 +19,10 @@ if st.button("Predict"):
     else:
         cleaned = clean_texts([user_review])
         vectorized = vectorizer.transform(cleaned)
-        
-        extra_features = np.array([[review_freq]])
-        extra_features_scaled = scaler.transform(extra_features)
 
-        final_input = hstack([vectorized, extra_features_scaled])
+        extra_feature = np.array([[1 if is_frequent else 0]])
+
+        final_input = hstack([vectorized, extra_feature])
 
         prediction = int(model.predict(final_input)[0])
         prob = model.predict_proba(final_input)[0][prediction]
@@ -33,4 +31,5 @@ if st.button("Predict"):
             st.success("Genuine Review")
         else:
             st.error("Fake Review")
+
         st.write(f"**Confidence**: {prob * 100:.2f}%")
